8caab8d9beb57c00887e6aa2b522641ee86f3851
athuu <athu091@gmail.com>
2021-08-11 08:52:19 +0530

    Created LoginSingup Fields

--- src/components/LoginSignUp.js
+++ src/components/LoginSignUp.js
@@ -1,5 +1,10 @@
  const LoginSigUp = () =>{
-     return <p>Login</p>
+     return <div style = {{border : '1px solid red',width : '250px'}}> 
+                <h4>Login or Signup </h4>
+                <input type= "textbox" />
+                <p>By Continuing, I agree to the <span>Terms of Use </span> & <span> Privacy Policy</span></p>
+                <button title = "Continue">Continue</button>
+            </div>
  }
 
  export default LoginSigUp;
