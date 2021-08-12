d5c03ba91e1cf5b91d4217b2582772526dfc0e40
athuu <athu091@gmail.com>
2021-08-12 23:57:58 +0530

    Style added as per myntra for login component

--- src/components/LoginSignUp.js
+++ src/components/LoginSignUp.js
@@ -1,9 +1,16 @@
  const LoginSigUp = () =>{
-     return <div style = {{border : '1px solid red',width : '250px'}}> 
+     return <div className = "Login-SignUp">
+         <div className = "img-Content">
+             <h5>Flat 50% Off</h5>
+             </div> 
                 <h4>Login or Signup </h4>
-                <input type= "textbox" />
-                <p>By Continuing, I agree to the <span>Terms of Use </span> & <span> Privacy Policy</span></p>
+                <div className = 'Text-Input'>
+                    <input type= "textbox" />
+                    <span >+91 |</span>
+                </div>
+                <p>By Continuing, I agree to the <b>Terms of Use </b> & <b> Privacy Policy</b></p>
                 <button title = "Continue">Continue</button>
+                <p> Have Troubbling login? <b>Get Help</b></p>
             </div>
  }
 
